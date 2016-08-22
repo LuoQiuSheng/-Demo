0a13f049e2104ec24ce4e031d62b08c03a8dee55
罗秋生 <luoqiusheng@luoqiushengdemacbook-pro.local>
2016-08-22 17:02:52 +0800

    COMMIT BY LUOQIUSHENG

--- 穷游/穷游/RecommendViewController.m
+++ 穷游/穷游/RecommendViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initializationNavigation];
+}
+
+- (void)initializationNavigation {
+    self.navigationController.navigationBarHidden = YES;
 }
 
 - (void)didReceiveMemoryWarning {
